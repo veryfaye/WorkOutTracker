d572a381ba2640e0e10d29e7112bfe93ad8e09e6
Amanda LeMoine <amanda1@mnlemoines.com>
2021-02-09 17:30:50 -0800

    html routes setup

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+htmlRoute = function (app) {
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
+
+module.exports = htmlRoute;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8080;
@@ -10,12 +11,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
 
-const db = require("./models");
-
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
